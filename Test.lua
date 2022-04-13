d76a6e08970865d28bea9cf1b76442b4aa15dced
Vatisco <quintencarlos@gmail.com>
2022-04-14 09:53:30 +1200

    Start

--- Test.lua
+++ Test.lua
@@ -1,3 +1,22 @@
-peripheral.find()
-print("test")
-print("test2")
+
+--Y level selector & excavate selector
+--Refuelling after dropping below 5000
+--dropping items into inventory behind start point
+--Recording start point
+--Returning to start once inventory is full and bedrock is reached
+--remembering start position
+
+function main()
+    term.clear()
+    term.setCursorPos(1,1)
+    print("Beginning Excavation program")
+    print("")
+    print("What Y level (and all blocks below)")
+    print("")
+    print("What dimensions?")
+    print("")
+
+
+end
+
+main()
